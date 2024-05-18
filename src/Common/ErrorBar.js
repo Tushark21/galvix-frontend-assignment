01a8d8a998e96a5cd3f88d5f5ab0dead09955ead
Tushar <tushargautamk21@gmail.com>
2024-05-18 16:17:57 +0530

    ErrorBar styled

--- src/Common/ErrorBar.js
+++ src/Common/ErrorBar.js
@@ -1,14 +1,20 @@
+import { Alert } from '@mui/material';
 import Snackbar from '@mui/material/Snackbar';
 
 function ErrorBar(props) {
     return (
         <Snackbar
             anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-            open={props.isError}
-            message="Some Error Occur"
-        />
+            open={props.isError}>
+            <Alert
+                severity="error"
+                variant="filled"
+                sx={{ width: '100%' }}
+            >
+                Some Error Occur
+            </Alert>
+        </Snackbar>
     );
 }
 
 export default ErrorBar;
-        
